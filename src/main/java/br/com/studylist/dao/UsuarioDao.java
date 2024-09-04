ea4bb61805dc495c1ecc5647eb8db00cde9a7609
Valéria Souza <valeriasilva1818@outlook.com>
2024-09-04 00:31:32 -0300

    study list primeira versão

---
+++ src/main/java/br/com/studylist/dao/UsuarioDao.java
@@ -0,0 +1,12 @@
+package br.com.studylist.dao;
+
+public class UsuarioDao {
+	
+	public void cadastrar () {
+		
+	}
+
+	public void listar () {
+		
+	}
+}
---
+++ src/main/java/br/com/studylist/model/Usuario.java
@@ -0,0 +1,75 @@
+package br.com.studylist.model;
+
+public class Usuario {
+	
+	private int id;
+	private String nome;
+	private String sobrenome;
+	private String email;
+	private String celular;
+	private String genero;
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(int id, String nome, String sobrenome, String email, String celular, String genero) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.celular = celular;
+		this.genero = genero;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+
+}
